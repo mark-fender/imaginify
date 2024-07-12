49a72a63295909ad79ed3d4cadf6c05a22959609
Marek Lipcak <98317473+mark-fender@users.noreply.github.com>
2024-07-12 15:42:46 +0200

    Update usePagination.ts

--- src/lib/hooks/usePagination.ts
+++ src/lib/hooks/usePagination.ts
@@ -2,11 +2,13 @@ import page from '@/app/(root)/page';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { formUrlQuery } from '../utils';
 
+type PaginationAction = 'prev' | 'next';
+
 const usePagination = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const pageChangeHandler = (action: string) => {
+  const pageChangeHandler = (action: PaginationAction) => {
     const pageValue = action === 'next' ? Number(page) + 1 : Number(page) - 1;
 
     const newUrl = formUrlQuery({
